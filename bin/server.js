2053574f0253ee13dcb932bf222060ce2b552efa
Benjamin Hudgens <benjamin@benjamindavid.com>
2020-06-25 04:43:03 -0500

    Actually hook up real functionality

--- bin/server.js
+++ bin/server.js
@@ -8,9 +8,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/diagnostic", (req, res) => res.status(200).end("OK"));
-app.all("*", (req, res) => req.query.url
+
+/** Specific bounce */
+app.get("/bounceme", (req, res) => req.query.url
   ? res.redirect(req.query.url)
   : res.status(400).json({ error: "no url" }));
+
+const _getRedirectUrl = req =>
+  `${process.env.AUTH_BOUNCE_URL}?url=${req.protocol}://${req.get('host')}${req.originalUrl}`;
+
+/** Auth bounce */
+app.all("*", (req, res) => process.env.AUTH_BOUNCE_URL
+  ? res.redirect(_getRedirectUrl(req))
+  : res.status(400).json({ error: "no AUTH_BOUNCE_URL" }));
 
 /*eslint no-process-env: "off"*/
 app.listen(process.env.PORT || 3000);
