d42b05f6fb6a09df1e2881f4e1c9519574070451
Brian Kelley <bkelley@glgroup.com>
2022-04-01 10:50:41 -0500

    preserve method here too

--- bin/server.js
+++ bin/server.js
@@ -20,7 +20,7 @@ app.get("/bounceme", (req, res) => {
     : `?_session=${req.cookies._session}`;
 
   return req.query.url
-    ? res.redirect(`${req.query.url}${_param}`)
+    ? res.redirect(307, `${req.query.url}${_param}`)
     : res.status(400).json({ error: "no url" });
 });
 
